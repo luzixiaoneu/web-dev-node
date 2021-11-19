75f753a499019fc349dbf758e5a6280b4a4f55e5
luzixiaoneu <lu.zix@northeastern.edu>
2021-11-19 12:50:02 -0500

    Initial commit

---
+++ services/profile-service.js
@@ -0,0 +1,24 @@
+let profile = require('../data/profile.json');
+
+module.exports = (app) => {
+	const getCurrentProfile = (req, res) => {
+		res.json(profile);
+	};
+
+	app.get("/api/profile", getCurrentProfile);
+
+
+	const updateCurrentProfile = (req, res) => {
+		const update = req.body;
+		profile.firstName = update.firstName;
+		profile.lastName = update.lastName;
+		profile.bio = update.bio;
+		profile.location = update.location;
+		profile.website = update.website;
+		profile.dateOfBirth = update.dateOfBirth;
+		res.json(profile)
+		console.log("body", profile);
+	}
+	app.put("/api/profile", updateCurrentProfile);
+
+};
